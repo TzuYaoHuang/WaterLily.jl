7ee924bef798befd5fdc9f54fa5364acf69130d2
Tzu-Yao Huang (Arch) <tzuyao.jason.huang@gmail.com>
2026-03-16 12:29:46 +0100

    New test to test on the boundary BC

--- example/tgv_test.jl
+++ example/tgv_test.jl
@@ -5,7 +5,24 @@ using NVTX
 
 println(pathof(WaterLily))
 
-function TGV3D(N; Re=Inf, We=Inf, λμ=1e-2, λρ=1e-3, T=Float64, mem=CuArray,pert=0.8,mp=true,perdir=(1,2,3))
+function TGV3D(N; Re=Inf, We=Inf, λμ=1e-2, λρ=1e-3, T=Float64, mem=CuArray,pert=0.,mp=true,perdir=(1,2,3))
+    NN = (N,N,N)
+    U = T(1)
+    κ=T(2π/N)
+    function uλ(i,xyz)
+        x,y,z = @. xyz*κ
+        i==1 && return -U*sin(x)*cos(y)*cos(z)
+        i==2 && return  U*cos(x)*sin(y)*cos(z)
+        return 0*U
+    end
+
+    return Simulation(
+        NN, (0, 0, 0), N;
+        T=T, uλ=uλ, mem=mem, perdir=(1,2,3)
+    )
+end
+
+function TGVpert3D(N; Re=Inf, We=Inf, λμ=1e-2, λρ=1e-3, T=Float64, mem=CuArray,pert=0.8,mp=true,perdir=(1,2,3))
     NN = (N,N,N)
     U = 1
     zeroT = T(0)
@@ -31,18 +48,13 @@ function prerunsim(N)
         mom_step!(sim.flow,sim.pois)
         print("$(i) ")
     end
+    println()
     return sim
 end
 
 function runsim!(sim)
-    for i ∈ 1:50
+    for i ∈ 1:500
         mom_step!(sim.flow,sim.pois)
-    end
-end
-
-function runsimNVTX!(sim)
-    for i ∈ 1:50
-        NVTX.@range "mom_step" mom_step!(sim.flow,sim.pois)
     end
 end
 
@@ -53,10 +65,10 @@ end
 
 sim = prerunsim(256)
 
-# @timev runsim!(sim)
+@timev runsim!(sim)
 
 Profile.init(n = 10^9, delay = 0.001)
 
 # @profview main()
 # @profview @timev runsim!(sim)
-CUDA.@profile external=true runsimNVTX!(sim)
+# CUDA.@profile external=true runsimNVTX!(sim)
